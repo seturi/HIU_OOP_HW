2caf6dcbcac98087bba15407481cd013a1e5b530
seturi <48719589+seturi@users.noreply.github.com>
2020-04-14 15:39:12 +0900

    HW1 - Project Rename

--- HW1/HW1/sum4.cpp
+++
@@ -1,7 +0,0 @@
-int sum(int numArray[], int length) {
-	if (length == 0) {	// 길이가 0 이면 0 리턴
-		return 0;
-	}
-	// 배열의 마지막 수를 더하고 탐색 길이를 1 줄여 sum 재귀호출
-	return sum(numArray, length - 1) + numArray[length - 1];
-}
